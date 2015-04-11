af22f10ee4d957231b9f565406bec1cbbf818b15
miguelespada <miguel.valero.espada@gmail.com>
2015-04-11 13:43:00 +0200

    restaurant model

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper" 
 describe Admin do
   describe "#admin?" do
-    it "can create a admin" do
+    it "has an admin factory" do
       FactoryGirl.create(:admin)
       expect(Admin.count).to eq 1
     end
