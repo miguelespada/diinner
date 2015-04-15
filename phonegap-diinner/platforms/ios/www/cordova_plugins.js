23b34da8a8f0607edd2ea5805cc050067b3d8631
jameshedaweng <heda.weng@gmail.com>
2015-04-15 13:28:49 +0200

    Add iOS platform

---
+++ phonegap-diinner/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-device/www/device.js",
+        "id": "cordova-plugin-device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-device": "1.0.0-dev"
+}
+// BOTTOM OF METADATA
+});
