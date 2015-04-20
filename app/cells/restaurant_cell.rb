4abb78a6c27b33b0c4b5a8e73ea6875b2fb1467c
jameshedaweng <heda.weng@gmail.com>
2015-04-20 18:13:52 +0200

    Merge

--- app/cells/restaurant_cell.rb
+++ app/cells/restaurant_cell.rb
@@ -1,29 +1,10 @@
 
 class RestaurantCell < BaseCell
   include ActionView::Helpers::DateHelper
-  def row
-    render
-  end
-
-  def dropdown_menu
-    render
-  end
-
-  def profile
-    render
-  end
+  
   private
   def status
-    "Inactive"
-  end
-
-  def logout_link
-    render
     last_activity = model.current_sign_in_at.nil? ? "never" : time_ago_in_words( model.current_sign_in_at )
     "Last time active: " + last_activity
-  end
-
-  def profile_link
-    render
   end
 end
