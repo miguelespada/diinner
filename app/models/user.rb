c29d22f414fd8c47f7f99b10ed4dfa50175a68b0
Rodrigo Crespo <rcrespocepeda@gmail.com>
2015-04-20 19:53:16 +0200

    user cannot login with email&password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,6 @@
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
-  # TODO: Do not copy&paste, use BDD (first test then feature)
-  # include Searchable
 
   field :email, type: String, default: ""
   field :image_url, type: String, default: ""
