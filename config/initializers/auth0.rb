097474217a7dbbd0ff1c4cb36b2feb874534e35b
miguelespada <miguel.valero.espada@gmail.com>
2015-04-25 14:08:58 +0200

    trying to fix Frozen array

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,9 +1,19 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider(
+  if Rails.env.development?
+    provider(
+        :auth0,
+        'ehMqgad1vB3TkYsH7qYXkVEqiBQgdrl5',
+        '4zrefWQz1QFtogYRMtGLONOEc2_VtQRAyEkYS0cb6iok0cdnK4IcudFTbwo-3PEB',
+        'rodcrespo.eu.auth0.com',
+        callback_path: "/auth/auth0/callback"
+    )
+  else Rails.env.production?
+    provider(
       :auth0,
-      'ehMqgad1vB3TkYsH7qYXkVEqiBQgdrl5',
-      '4zrefWQz1QFtogYRMtGLONOEc2_VtQRAyEkYS0cb6iok0cdnK4IcudFTbwo-3PEB',
-      'rodcrespo.eu.auth0.com',
+      'WQpHmQ3d6SpZXTiTz4iTPr5N8MA0QPvl',
+      '1MAFjnDVaimdhM7nDUgP5vpcffUPOS8xwbIAuGCs-fPyRNh_68c44jpxfT5YAx5M',
+      'espadaysantacruz.auth0.com',
       callback_path: "/auth/auth0/callback"
-  )
+    )
+  end
 end
