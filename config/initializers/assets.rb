2cbadf7cf27bcf9c4f9dc73a32c9a5a8100124f2
miguelespada <miguel.valero.espada@gmail.com>
2015-04-25 12:11:22 +0200

    trying auth0 production

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,6 +4,7 @@
 Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( ie.js )
+Rails.application.config.assets.precompile += %w( auth0.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
