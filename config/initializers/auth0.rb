3267bae43ad1eaec4d8567dc028d2b4a994ecd0e
miguelespada <miguel.valero.espada@gmail.com>
2015-04-28 14:54:56 +0200

    change Auth0 secret

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     provider(
         :auth0,
         'BBLhUOEcSMo60RljToZKPWzNClPnAOPl',
-        '4zrefWQz1QFtogYRMtGLONOEc2_VtQRAyEkYS0cb6iok0cdnK4IcudFTbwo-3PEB',
+        '5qeGPCT17bqUzqANM-13_dHP26nEyw0XrVwSutrO6ZS_sbszJIgDA_nit5Xx6v2u',
         'app35743745.eu.auth0.com',
         callback_path: "/auth/auth0/callback"
     )
