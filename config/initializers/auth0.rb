f9714b9b31ea5c06fa8f950b85867040442d1562
Rodrigo Crespo <rcrespocepeda@gmail.com>
2015-04-28 12:41:40 +0200

    auth0 login try

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -1,19 +1,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  if Rails.env.development?
     provider(
         :auth0,
         'BBLhUOEcSMo60RljToZKPWzNClPnAOPl',
-        '5qeGPCT17bqUzqANM-13_dHP26nEyw0XrVwSutrO6ZS_sbszJIgDA_nit5Xx6v2u',
+        '4zrefWQz1QFtogYRMtGLONOEc2_VtQRAyEkYS0cb6iok0cdnK4IcudFTbwo-3PEB',
         'app35743745.eu.auth0.com',
-        callback_path: "/callback"
+        callback_path: "/auth/auth0/callback"
     )
-  else Rails.env.production?
-  provider(
-      :auth0,
-      ENV['AUTH0_CLIENT_ID'],
-      ENV['AUTH0_CLIENT_SECRET'],
-      ENV['AUTH0_DOMAIN'],
-      :callback_path => "/callback"
-  )
-  end
 end
