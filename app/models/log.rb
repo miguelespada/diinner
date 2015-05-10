da0ac5a2f4d443ec56f67b34f4fcb7e7b8430d46
miguelespada <miguel.valero.espada@gmail.com>
2015-05-10 19:16:52 +0200

    some refactor

--- app/models/log.rb
+++ app/models/log.rb
@@ -7,6 +7,7 @@ class Log
   field :name,    type: String
   field :entity_id,    type: String
 
+
   def get_entity
     @entity ||= type.classify.safe_constantize.find(entity_id)
   end
--- app/models/test.rb
+++ app/models/test.rb
@@ -2,9 +2,12 @@ class Test
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :question, type: String, default: ""
-  field :caption_A, type: String, default: ""
-  field :caption_B, type: String, default: ""
+  field :question, type: String
+  field :caption_A, type: String
+  field :caption_B, type: String
+
+  validates_presence_of :question, :caption_A, :caption_B
+
   has_attachment :option_A, accept: [:jpg, :png, :gif]
   has_attachment :option_B, accept: [:jpg, :png, :gif]
   # TODO: do not add features without a test
