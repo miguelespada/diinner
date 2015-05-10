0a0dbc188fd3d862cdc025d28de7daf1d4286b6b
miguelespada <miguel.valero.espada@gmail.com>
2015-05-10 17:56:59 +0200

    add log pagination

--- app/controllers/admin/logs_controller.rb
+++ app/controllers/admin/logs_controller.rb
@@ -1,7 +1,7 @@
 class  Admin::LogsController < AdminController
 
   def index
-    @logs = Log.all
+    @logs = Log.desc(:created_at).page params[:page]
   end
 
 end
--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -1,8 +1,5 @@
-Kaminari::Hooks.init
-Elasticsearch::Model::Response::Response.__send__ :include, Elasticsearch::Model::Response::Pagination::Kaminari
-
 Kaminari.configure do |config|
-  config.default_per_page = 10
+   config.default_per_page = 25
   # config.max_per_page = nil
   # config.window = 4
   # config.outer_window = 0
