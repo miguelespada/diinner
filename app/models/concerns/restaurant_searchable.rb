f4a0117c296972b5f5cc55600c21148ba600a795
Rodrigo Crespo <rcrespocepeda@gmail.com>
2015-05-15 13:52:06 +0200

    indexes ES

--- app/models/concerns/restaurant_searchable.rb
+++ app/models/concerns/restaurant_searchable.rb
@@ -6,14 +6,19 @@ module RestaurantSearchable
     include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
 
-    index_name  "restaurants-#{Rails.env}"
+    index_name  "diinner-restaurant-#{Rails.env}"
 
     def as_indexed_json(options={})
-      as_json(only: 'name')
+      as_json(only: [:name, :email, :description, :phone, :address, :city])
     end
 
     mapping do
       indexes :name, type: :string, :analyzer => :spanish, :boost => 50
+      indexes :email, type: :string, :analyzer => :spanish, :boost => 50
+      indexes :description, type: :string, :analyzer => :spanish, :boost => 40
+      indexes :phone, type: :string, :analyzer => :spanish, :boost => 50
+      indexes :address, type: :string, :analyzer => :spanish, :boost => 50
+      indexes :city, type: :string, :analyzer => :spanish, :boost => 20
     end
 
   end
--- app/models/concerns/user_searchable.rb
+++ app/models/concerns/user_searchable.rb
@@ -6,14 +6,18 @@ module UserSearchable
     include Elasticsearch::Model
     include Elasticsearch::Model::Callbacks
 
-    index_name  "users-#{Rails.env}"
+    index_name  "diinner-user-#{Rails.env}"
 
     def as_indexed_json(options={})
-      as_json(only: 'name')
+      as_json(only: [:name, :email, :image_url, :birth, :gender])
     end
 
     mapping do
       indexes :name, type: :string, :analyzer => :spanish, :boost => 50
+      indexes :email, type: :string, :boost => 50
+      indexes :image_url, type: :string, :boost => 40
+      indexes :birth, type: :date, :boost => 50
+      indexes :gender, type: :string, :boost => 50
     end
   end
 end
