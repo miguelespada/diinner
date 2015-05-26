a0b499acde8fd75b408829da83824e0b1213d2e4
miguelespada <miguel.valero.espada@gmail.com>
2015-05-26 12:20:54 +0200

    refactor suggestion engine

--- app/services/suggestion_engine.rb
+++ app/services/suggestion_engine.rb
@@ -1,10 +1,31 @@
 class SuggestionEngine
-  def initialize user
+  def initialize user, params
     @user = user
+    @params = params
   end
 
-  def search date, price, companies = []
-    # TODO here should be the magic
+    # TODO maybe push to suggestion engine
+  def companies
+    company = []
+    @params[:companies_attributes].each do |company_params|
+      c = company_params[1]
+      if !c[:age].blank? && !c[:gender].blank?
+        company << Company.new(c)
+      end
+    end
+    company
+  end
+
+  def date
+    Date.strptime @params[:date]
+  end
+
+  def price
+    price = @params[:price].to_i
+  end
+
+
+  def search
     results = []
     Table.where(:date => date).each do |table|
       reservation = Reservation.new({user: @user,
