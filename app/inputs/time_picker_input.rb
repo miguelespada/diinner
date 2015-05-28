c4433bc5e9fc2d37eaa08c4d766ec184087ac9e1
miguelespada <miguel.valero.espada@gmail.com>
2015-05-28 12:04:11 +0200

    solving cucumber issue with hidden fields

--- app/inputs/time_picker_input.rb
+++ app/inputs/time_picker_input.rb
@@ -12,4 +12,12 @@ class TimePickerInput < DatePickerInput
   def date_options
     date_options_base
   end
+
+   def input_button
+    template.content_tag :span, class: 'input-group-btn' do
+      template.content_tag :button, class: 'btn btn-default', type: 'button' do
+        template.content_tag :span, '', class: 'glyphicon glyphicon-time'
+      end
+    end
+  end
 end
