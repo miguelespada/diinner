c53b4cd7c25d6f942f500e19374be810860d982b
miguelespada <miguel.valero.espada@gmail.com>
2015-05-31 15:33:06 +0200

    add table manager service

--- spec/models/table_manager_spec.rb
+++ spec/models/table_manager_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 describe TableManager do
-  before(:each) do
+  before(:all) do
     @user = FactoryGirl.create(:user)
     @restaurant = FactoryGirl.create(:restaurant, :with_tables)
     @table = @restaurant.tables.first
@@ -18,6 +18,10 @@ describe TableManager do
   it "returns today tables" do
     FactoryGirl.create(:table, date: Date.tomorrow)
     expect(Table.count).to eq 2
+    expect(TableManager.today_tables.count).to eq 1
+  end
+
+   it "returns today tables" do
     expect(TableManager.today_tables.count).to eq 1
   end
 
