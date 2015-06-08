7ca991b47857b186e6feb45427c60ead0e5b06ed
miguelespada <miguel.valero.espada@gmail.com>
2015-06-08 16:40:56 +0200

    solve file issue

--- IonicApp/app/js/app.js
+++ IonicApp/app/js/app.js
@@ -9,6 +9,7 @@ angular.module('starter', ['ionic', 'ngResource']
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
+
     if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
     }
