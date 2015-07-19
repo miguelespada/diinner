4330cee057db6efdf4b9412531a0273cd560a1a8
Rodrigo Crespo <rcrespocepeda@gmail.com>
2015-07-19 22:07:32 +0200

    query fix

--- IonicApp/app/scripts/helpers/services.js
+++ IonicApp/app/scripts/helpers/services.js
@@ -3,46 +3,53 @@
 dinnerApp.service('UserManager',['$resource', 'ENV', 'store', function($resource, ENV, store) {
   this.getUser = function() {
     var token = store.get('token');
-    console.log(token);
     return $resource(ENV.apiEndPoint + '/user.json').get({user_token: token})
   };
 
   this.updateUser = function(user) {
-    return $resource(ENV.apiEndPoint + '/user').save({user: user});
+    var token = store.get('token');
+    return $resource(ENV.apiEndPoint + '/user').save({user_token: token, user: user});
   };
 
   this.getNotifications = function() {
-    return $resource(ENV.apiEndPoint + '/notifications.json').get()
+    var token = store.get('token');
+    return $resource(ENV.apiEndPoint + '/notifications.json').get({user_token: token});
   };
 
   this.getReservations = function() {
-    return $resource(ENV.apiEndPoint + '/reservations.json').get()
+    var token = store.get('token');
+    return $resource(ENV.apiEndPoint + '/reservations.json').get({user_token: token})
   };
 
   this.cancelReservation = function(reservation_id) {
-    return $resource(ENV.apiEndPoint + '/cancel_reservation').save({reservation_id: reservation_id})
+    var token = store.get('token');
+    return $resource(ENV.apiEndPoint + '/cancel_reservation').save({user_token: token, reservation_id: reservation_id})
   };
 
   this.reserve = function(reservation) {
-    return $resource(ENV.apiEndPoint + '/reserve').save({reservation: reservation})
+    var token = store.get('token');
+    return $resource(ENV.apiEndPoint + '/reserve').save({user_token: token, reservation: reservation})
   };
 
   this.updateCustomer = function(payment_token) {
-    return $resource(ENV.apiEndPoint + '/update_customer').save({payment_token: payment_token})
+    var token = store.get('token');
+    return $resource(ENV.apiEndPoint + '/update_customer').save({user_token: token, payment_token: payment_token})
   };
 }]);
 
 
-dinnerApp.service('CityManager',['$resource', 'ENV', function($resource, ENV) {
+dinnerApp.service('CityManager',['$resource', 'ENV', 'store', function($resource, ENV, store) {
   this.getCities = function() {
-    return $resource(ENV.apiEndPoint + '/cities.json').get();
+    var token = store.get('token');
+    return $resource(ENV.apiEndPoint + '/cities.json').get({user_token: token});
   };
 }]);
 
 
-dinnerApp.service('TableManager',['$resource', 'ENV', function($resource, ENV) {
+dinnerApp.service('TableManager',['$resource', 'ENV', 'store', function($resource, ENV, store) {
   this.searchTables = function(filters) {
-    return $resource(ENV.apiEndPoint + '/table/search').get({filters: filters});
+    var token = store.get('token');
+    return $resource(ENV.apiEndPoint + '/table/search').get({user_token: token, filters: filters});
   };
 }]);
 
