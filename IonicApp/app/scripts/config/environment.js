d56ab2dad6e7d5ecff6014ac029f0b530f1bbd96
jameshedaweng <heda.weng@gmail.com>
2015-07-21 13:12:08 +0200

    add in app browser to ios

--- IonicApp/app/scripts/config/environment.js
+++ IonicApp/app/scripts/config/environment.js
@@ -2,6 +2,6 @@
 
  angular.module('environment', [])
 
-.constant('ENV', {name:'production',apiEndPoint:'http://diinner.herokuapp.com/ionic',stripePublishableKey:'pk_test_aVle0kDCMX2iyIfuEvHoYcfq',auth0ClientID:'BBLhUOEcSMo60RljToZKPWzNClPnAOPl',auth0Domain:'app35743745.eu.auth0.com'})
+.constant('ENV', {name:'development',apiEndPoint:'http://localhost:3000/ionic',stripePublishableKey:'pk_test_aVle0kDCMX2iyIfuEvHoYcfq',auth0ClientID:'BBLhUOEcSMo60RljToZKPWzNClPnAOPl',auth0Domain:'app35743745.eu.auth0.com'})
 
 ;
