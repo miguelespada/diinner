4cb25b64b098bb88df1f22df71c1466f5d871f8c
Rodrigo Crespo <rcrespocepeda@gmail.com>
2015-10-10 12:02:56 +0200

    accessory bar iOS

--- IonicApp/app/scripts/app.js
+++ IonicApp/app/scripts/app.js
@@ -18,11 +18,6 @@ var dinnerApp = angular.module(
 dinnerApp.run(function($ionicPlatform) {
 
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
       StatusBar.styleLightContent();
