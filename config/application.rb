8eef7a2dd364bab9efdf3b55b64cd6129a7749a9
Meriem El Yamri <melyamri@gmail.com>
2016-02-01 20:34:36 +0100

    literals

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,7 @@ module Dinner
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :es
+    config.i18n.default_locale = :es
     config.time_zone = 'Europe/Madrid'
 
     # heroku config:add TZ="Europe/Madrid"
