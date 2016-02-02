201d23f5e645890af3ca722f99313c6cd2593012
miguelespada <miguel.valero.espada@gmail.com>
2016-02-02 13:14:45 +0100

    add (commented) autho0 credentials

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -7,3 +7,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
         callback_path: "/auth/auth0/callback"
     )
 end
+
+# Rails.application.config.middleware.use OmniAuth::Builder do
+#   provider(
+#     :auth0,
+#     '6gY7oD4V2IMyzdhBSfcuVJKwRv5IosUt',
+#     'mxAdLQyuoDBDgLC-4QXFANkZEFu-u7GZAXNYf3OG4a5JvesiuLkC5uat_kdzpfSF',
+#     'diinner.eu.auth0.com',
+#     callback_path: "/auth/auth0/callback"
+#   )
+# end
