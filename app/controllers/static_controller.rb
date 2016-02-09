7a65ba4d7283119af41314713061180a1a5df736
Rodrigo Crespo <rcrespocepeda@gmail.com>
2016-02-09 19:30:48 +0100

    landing redirect if authenticated

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,5 +1,7 @@
 class StaticController < ApplicationController
   layout "static"
+
+  before_action :redirect_if_authenticated, only: [:index]
   caches_action :index
   
   def index
@@ -22,6 +24,16 @@ class StaticController < ApplicationController
   end
 
   def drop_out
+  end
+
+  private
+
+  def redirect_if_authenticated
+    @session ||= UserSession.new(session)
+    if @session.logged?
+      @current_user = @session.user_from_session
+      redirect_to user_path(@current_user)
+    end
   end
 
 
