104f1d9fa865cde796b6bb0b0e660a0ce5808c4f
miguelespada <miguel.valero.espada@gmail.com>
2016-02-19 10:16:14 +0100

    fixing I logout test

--- features/step_definitions/common_steps.rb
+++ features/step_definitions/common_steps.rb
@@ -12,6 +12,9 @@ end
 When(/^I logout$/) do
   click_on "Salir"
   visit auth_logout_path
+  allow_any_instance_of(UserSession).to receive(:logged?).and_return(false)
+  allow_any_instance_of(UserSession).to receive(:user_from_session).and_return(nil)
+  visit root_path
 end
 
 Then(/^I should see the homepage$/) do
