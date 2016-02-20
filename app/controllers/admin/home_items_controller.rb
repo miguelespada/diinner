b54d0558ca7fc122a577003632abcd87003a3971
Meriem El Yamri <melyamri@gmail.com>
2016-02-20 12:03:28 +0100

    profile mobile background

--- app/controllers/admin/home_items_controller.rb
+++ app/controllers/admin/home_items_controller.rb
@@ -15,6 +15,7 @@ class  Admin::HomeItemsController < AdminController
   def home_item_params
     params.require(:home_item).permit(:header,
                                       :mobile_background,
+                                      :mobile_profile_background,
                                       :restaurant_sample_1_id,
                                       :restaurant_sample_2_id,
                                       :restaurant_sample_3_id,
