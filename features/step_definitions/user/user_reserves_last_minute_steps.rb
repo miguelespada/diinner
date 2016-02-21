fdc70675d5907b2119479316421d3f9fc049e77e
miguelespada <miguel.valero.espada@gmail.com>
2016-02-21 18:59:57 +0100

    last minute reservation feature js

---
+++ features/step_definitions/user/user_reserves_last_minute_steps.rb
@@ -0,0 +1,64 @@
+Given(/^There is a last minute plan for today$/) do
+  @city = FactoryGirl.create(:city)
+
+  @restaurant = FactoryGirl.create(:restaurant, city: @city)
+  @restaurant.menus.create(FactoryGirl.build(:menu).attributes)
+  @menu = @restaurant.menus.first
+  @restaurant.tables.create(FactoryGirl.build(:table, :for_today, menu: @menu).attributes)
+  @table = @restaurant.tables.first
+
+  he = FactoryGirl.create(:user, :with_customer_id,  gender: :male)
+  she = FactoryGirl.create(:user, :with_customer_id,  gender: :female)
+
+  FactoryGirl.create(:reservation, user: he, table: @table, date: @table.date)
+  FactoryGirl.create(:reservation, user: he, table: @table, date: @table.date)
+  FactoryGirl.create(:reservation, user: she, table: @table, date: @table.date)
+  FactoryGirl.create(:reservation, user: she, table: @table, date:@table.date)
+  
+  return_value = Hash.new
+  return_value[:id] = "123"
+  allow_any_instance_of(Reservation).to receive(:create_stripe_charge) do |entity|
+    entity.user.customer ? return_value : nil
+  end
+  allow_any_instance_of(Reservation).to receive(:stripe_capture).and_return return_value
+  allow_any_instance_of(Reservation).to receive(:stripe_refund).and_return return_value 
+
+  TableManager.process_today_tables
+
+  allow(DateTime).to receive(:now).and_return DateTime.now.change({ hour: 12, min: 00, sec: 00 })
+end
+
+Given(/^I have preferences$/) do
+  @user.preference = Preference.new
+  @user.preference.city = @city
+  @user.save!
+end
+
+
+
+When(/^I search a plan for today$/) do
+
+  step "I go to the user page"
+  find("#new-reservation-link").trigger("click")
+  find(".today").trigger("click")
+  click_on "Buscar mesas"
+  click_on "Reservar"
+
+  step("I fill in the credit card with valid details")
+
+  find("#continuar").trigger("click")
+  sleep(1)
+  expect(page).to have_content("El estado del plan es RESERVADO")
+  expect(page).to have_content("Antes de las 18h00 te comunicaremos si tu plan se confirma.")
+  expect(page).to have_content("Lo sentimos, no es posible cancelar las reservas en el mismo día del plan.")
+  visit @user_path
+  within(".my-reservations") do
+    expect(page).to have_content(@restaurant.name)
+  end
+end
+
+Then(/^I can see the new reservation notification$/) do
+  click_on "Notificaciones"
+  expect(page).to have_content("Tu plan diinner en el restaurante #{@restaurant.name} el día #{@table.date} se ha reservado correctamente.") 
+end
+
