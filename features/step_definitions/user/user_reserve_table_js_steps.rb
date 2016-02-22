d45faad8d88b653b372c56fa9fa6ba38a14b48f7
Rodrigo Crespo <rcrespocepeda@gmail.com>
2016-02-22 21:19:59 +0100

    company 1 an 2 tests

--- features/step_definitions/user/user_reserve_table_js_steps.rb
+++ features/step_definitions/user/user_reserve_table_js_steps.rb
@@ -32,6 +32,40 @@ When(/^I reserve a table for tomorrow$/) do
   find("#continuar").trigger("click")
 end
 
+When(/^I reserve a table for tomorrow with company 1$/) do
+
+  expect(EmailNotifications).to receive(:notify_new_reservation).exactly(1).times
+  step("I search a table with company 1")
+  within ".search-results" do
+    expect(page).to have_content(@restaurant.name)
+    expect(page).to have_content(@menu.name)
+    expect(page).to have_content(@menu.price)
+    expect(page).to have_content(@menu.price * 2)
+  end
+
+  click_on "reserve-#{@restaurant.name}"
+  step("I fill in the credit card with valid details")
+  expect(page).to have_content("Número de menús: 2")
+  find("#continuar").trigger("click")
+end
+
+When(/^I reserve a table for tomorrow with company 2$/) do
+
+  expect(EmailNotifications).to receive(:notify_new_reservation).exactly(1).times
+  step("I search a table with company 2")
+  within ".search-results" do
+    expect(page).to have_content(@restaurant.name)
+    expect(page).to have_content(@menu.name)
+    expect(page).to have_content(@menu.price)
+    expect(page).to have_content(@menu.price * 3)
+  end
+
+  click_on "reserve-#{@restaurant.name}"
+  step("I fill in the credit card with valid details")
+  expect(page).to have_content("Número de menús: 3")
+  find("#continuar").trigger("click")
+end
+
 When(/^I reserve a table for any day$/) do
 
   expect(EmailNotifications).to receive(:notify_new_reservation).exactly(1).times
@@ -61,6 +95,34 @@ When(/^I search a table with default values$/) do
   click_on "Buscar mesas"
 end
 
+When(/^I search a table with company 1$/) do
+  step "I go to the user page"
+  find("#new-reservation-link").trigger("click")
+  find(".company-toggle[data-company='1']").trigger("click")
+  find(".toggle[data-number='1']").trigger("click")
+  within "#friend_0" do
+    find(".friend-toggle[data-gender='male']").trigger("click")
+    fill_in "Edad", with: "30"
+  end
+  click_on "Buscar mesas"
+end
+
+When(/^I search a table with company 2$/) do
+  step "I go to the user page"
+  find("#new-reservation-link").trigger("click")
+  find(".company-toggle[data-company='1']").trigger("click")
+  find(".toggle[data-number='2']").trigger("click")
+  within "#friend_0" do
+    find(".friend-toggle[data-gender='male']").trigger("click")
+    fill_in "Edad", with: "30"
+  end
+  within "#friend_1" do
+    find(".friend-toggle[data-gender='female']").trigger("click")
+    fill_in "Edad", with: "27"
+  end
+  click_on "Buscar mesas"
+end
+
 Then(/^I fill in the credit card with valid details$/) do
   fill_in "card_holder", with: "Rodrigo Rato"
   fill_in "card_number", with: "4012888888881881"
@@ -80,4 +142,28 @@ Then(/^I see the table details$/) do
   expect(@user.reservations.first.is_last_minute?).to eq false
   click_on "Notificaciones"
   expect(page).to have_content("Tu plan diinner en el restaurante #{@restaurant.name} el día #{@table.date} se ha reservado correctamente.") 
+end
+
+Then(/^I see the table details with company 1$/) do
+  sleep(1)
+  expect(page).to have_content("El estado del plan es RESERVADO")
+  expect(page).to have_content("Número de menús comprados: 2")
+  expect(@user.reservations.count).to eq 1
+  expect(@user.notifications.count).to eq 1
+  expect(@user.reservations.first.cancelled?).to eq false
+  expect(@user.reservations.first.is_last_minute?).to eq false
+  click_on "Notificaciones"
+  expect(page).to have_content("Tu plan diinner en el restaurante #{@restaurant.name} el día #{@table.date} se ha reservado correctamente.")
+end
+
+Then(/^I see the table details with company 2$/) do
+  sleep(1)
+  expect(page).to have_content("El estado del plan es RESERVADO")
+  expect(page).to have_content("Número de menús comprados: 3")
+  expect(@user.reservations.count).to eq 1
+  expect(@user.notifications.count).to eq 1
+  expect(@user.reservations.first.cancelled?).to eq false
+  expect(@user.reservations.first.is_last_minute?).to eq false
+  click_on "Notificaciones"
+  expect(page).to have_content("Tu plan diinner en el restaurante #{@restaurant.name} el día #{@table.date} se ha reservado correctamente.")
 end
--- features/user/user_reserves_table_js.feature
+++ features/user/user_reserves_table_js.feature
@@ -17,3 +17,15 @@ Feature: User Reserves table
     Given There are some available tables for any day
     When I reserve a table for any day
     Then I see the table details
+
+  @user_reserves_table_with_company_1
+  Scenario:
+    Given There are some available tables for tomorrow
+    When I reserve a table for tomorrow with company 1
+    Then I see the table details with company 1
+
+  @user_reserves_table_with_company_2
+  Scenario:
+    Given There are some available tables for tomorrow
+    When I reserve a table for tomorrow with company 2
+    Then I see the table details with company 2
